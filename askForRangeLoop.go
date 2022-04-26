3964c2849b24015ddae160e545da09851a6fd0f8
vinnow98 <evinwoon@gmail.com>
2022-04-26 19:31:01 +0800

    Completed!

--- askForRangeLoop.go
+++ askForRangeLoop.go
@@ -1,5 +1,29 @@
 package main
 
+import "fmt"
+
 func main() {
-	//Insert code for only even here
+	first := 0
+	second := 0
+	tempVal := 0
+	fmt.Println("1st number")
+	fmt.Scanln(&first)
+	fmt.Println("2nd number")
+	fmt.Scanln(&second)
+	if second < first {
+		tempVal = second
+		second = first
+		first = tempVal
+
+	}
+	for i := first; i <= second; i++ {
+		if i%2 == 1 {
+			fmt.Print("\nThis number is odd ", i)
+		}
+	}
+	for i := first; i <= second; i++ {
+		if i%2 == 0 {
+			fmt.Print("\nThis number is even ", i)
+		}
+	}
 }
